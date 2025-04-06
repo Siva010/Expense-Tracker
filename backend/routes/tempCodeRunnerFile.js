cd5efb0c2750594a71111932bcfb5d757acefb0a
Siva010 <ms.ivas010@gmail.com>
2025-04-06 15:44:47 +0530

    apis

---
+++ backend/routes/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+console.log(typeof registerUser); // should be 'function'
+console.log(typeof loginUser); // should be 'function'
+console.log(typeof getUserInfo); // should be 'function'
+console.log(typeof protect); // should be 'function'
